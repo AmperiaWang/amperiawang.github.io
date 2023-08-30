5903036feaad1ca433eb75d180be5726f48816f2
mint <2015074432@qq.com>
2023-08-31 02:34:02 +0800

    update tarot

---
+++ tarot/assets/card_info.js
@@ -0,0 +1,48 @@
+var card_info = [
+    {
+        "name": "大阿卡纳牌",
+        "folder": "akana",
+        "cards": [
+            {
+                "name": "愚人",
+                "name_english": "The Fool",
+                "image_file": "fool",
+                "symbol": "O",
+                "title_2": ["愚", "人"],
+                "keywords": ["开始", "自由", "天真", "流浪"]
+            },
+            {
+                "name": "魔术师",
+                "name_english": "The Magician",
+                "image_file": "magician",
+                "symbol": "I",
+                "title_2": ["术", "士"],
+                "keywords": ["观察", "开始", "创造"]
+            },
+            {
+                "name": "女教主",
+                "name_english": "The High Priestess",
+                "image_file": "high-priestess",
+                "symbol": "II",
+                "title_2": ["教", "主"],
+                "keywords": ["等待", "智慧", "理性", "消极", "静止"]
+            },
+            {
+                "name": "皇后",
+                "name_english": "The Empress",
+                "image_file": "empress",
+                "symbol": "III",
+                "title_2": ["皇", "后"],
+                "keywords": ["等待", "智慧", "理性", "消极", "静止"]
+            },
+            {
+                "name": "皇后",
+                "name_english": "The Empress",
+                "image_file": "empress",
+                "symbol": "III",
+                "title_2": ["皇", "后"],
+                "keywords": ["等待", "智慧", "理性", "消极", "静止"]
+            },
+        ]
+    }
+];
