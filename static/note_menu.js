3f4dc6217fa24d41d4d668a1caf24cc96ad04738
mint <2015074432@qq.com>
2026-01-04 15:49:55 +0800

    Index page to Vue

---
+++ static/note_menu.js
@@ -0,0 +1,40 @@
+const note_menu = [
+    {
+        "title": "数字信号处理",
+        "path": "note/DSP",
+        "subs": [
+            {
+                "title": "1 傅里叶变换、拉普拉斯变换和z变换",
+                "path": "1.md"
+            },
+            {
+                "title": "2 离散傅里叶变换与快速傅里叶变换",
+                "path": "2.md"
+            },
+            {
+                "title": "3 滤波器",
+                "path": "3.md"
+            }
+        ]
+    },
+    {
+        "title": "当前研究内容",
+        "path": "note/SNN",
+        "subs": [
+            {
+                "title": "1 脉冲神经元模型",
+                "path": "1.md"
+            }
+        ]
+    },
+    {
+        "title": "小工具",
+        "path": "utils",
+        "subs": [
+            {
+                "title": "小工具",
+                "path": "README.md"
+            }
+        ]
+    }
+];
